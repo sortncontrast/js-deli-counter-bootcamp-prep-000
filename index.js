77b6b6303d1100d84d5ec360146e78879a10e0c9
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-26 00:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ function nowServing(array) {
 function currentLine(array) {
   if(array.length > 0) {
     let newArray = [];
-    let counter = 1;
+    let counter = 0;
     while(counter <= array.length) {
-      newArray.push(" " + (counter) + ". " + array[counter]);
+      newArray.push(" " + (counter+1) + ". " + array[counter]);
       counter += 1;
     }return ("The line is currently:" + newArray);
   }else {
